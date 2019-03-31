592c7446a5025787bb589b64b4a50c4f1ea8cf28
Ganesh <ganesh.nanduru@gmail.com>
2019-03-31 12:57:06 -0400

    Chessboard finished

--- ChessDriver.java
+++ ChessDriver.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 public class ChessDriver{
    public static void main(String[] args){
       JFrame frame = new JFrame("Chessboard");
-      frame.setSize(820,853);
+      frame.setSize(820,855);
       frame.setLocation(100,100);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.setContentPane(new ChessPanel());
